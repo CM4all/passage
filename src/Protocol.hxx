013e8f65e8e37ed451585544a340eb0c33437a28
Max Kellermann <max.kellermann@gmail.com>
2017-09-07 09:41:48 +0200

    Protocol: add IsValidCommandChar()

--- src/Protocol.hxx
+++ src/Protocol.hxx
@@ -9,6 +9,12 @@
 
 #include <stdexcept>
 
+static constexpr bool
+IsValidCommandChar(char ch)
+{
+	return IsAlphaNumericASCII(ch);
+}
+
 static void
 CheckCommand(StringView s)
 {
@@ -16,6 +22,6 @@ CheckCommand(StringView s)
 		throw std::runtime_error("Empty command");
 
 	for (char ch : s)
-		if (!IsAlphaNumericASCII(ch))
+		if (!IsValidCommandChar(ch))
 			throw std::runtime_error("Malformed command");
 }
