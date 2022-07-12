eee007485f1fe89e62f3f2e2cb422e3b1b180b6f
Max Kellermann <mk@cm4all.com>
2022-07-12 20:18:35 +0200

    test/TestParser: indent with tabs

--- test/TestParser.cxx
+++ test/TestParser.cxx
@@ -10,63 +10,63 @@
 
 TEST(Parser, Simple)
 {
-    const auto e = ParseEntity("OK\n\n");
-    EXPECT_EQ(e.command, "OK");
-    EXPECT_TRUE(e.args.empty());
-    EXPECT_TRUE(e.headers.empty());
+	const auto e = ParseEntity("OK\n\n");
+	EXPECT_EQ(e.command, "OK");
+	EXPECT_TRUE(e.args.empty());
+	EXPECT_TRUE(e.headers.empty());
 }
 
 TEST(Parser, OneArg)
 {
-    const auto e = ParseEntity("FOO one\n");
-    EXPECT_EQ(e.command, "FOO");
-    EXPECT_FALSE(e.args.empty());
-    EXPECT_EQ(e.args.front(), "one");
-    EXPECT_EQ(std::next(e.args.begin()), e.args.end());
-    EXPECT_TRUE(e.headers.empty());
+	const auto e = ParseEntity("FOO one\n");
+	EXPECT_EQ(e.command, "FOO");
+	EXPECT_FALSE(e.args.empty());
+	EXPECT_EQ(e.args.front(), "one");
+	EXPECT_EQ(std::next(e.args.begin()), e.args.end());
+	EXPECT_TRUE(e.headers.empty());
 }
 
 TEST(Parser, TwoArgs)
 {
-    const auto e = ParseEntity("FOO one two");
-    EXPECT_EQ(e.command, "FOO");
-    EXPECT_FALSE(e.args.empty());
-    EXPECT_EQ(e.args.front(), "one");
-    EXPECT_EQ(*std::next(e.args.begin()), "two");
-    EXPECT_EQ(std::next(e.args.begin(), 2), e.args.end());
-    EXPECT_TRUE(e.headers.empty());
+	const auto e = ParseEntity("FOO one two");
+	EXPECT_EQ(e.command, "FOO");
+	EXPECT_FALSE(e.args.empty());
+	EXPECT_EQ(e.args.front(), "one");
+	EXPECT_EQ(*std::next(e.args.begin()), "two");
+	EXPECT_EQ(std::next(e.args.begin(), 2), e.args.end());
+	EXPECT_TRUE(e.headers.empty());
 }
 
 TEST(Parser, Headers)
 {
-    const auto e = ParseEntity("FOO\n"
-                               "abc: 1\n"
-                               "def: 2");
-    EXPECT_EQ(e.command, "FOO");
-    EXPECT_TRUE(e.args.empty());
-    EXPECT_FALSE(e.headers.empty());
-    EXPECT_EQ(std::distance(e.headers.begin(), e.headers.end()), 2u);
-    EXPECT_EQ(e.headers.find("abc")->second, "1");
-    EXPECT_EQ(e.headers.find("def")->second, "2");
+	const auto e = ParseEntity("FOO\n"
+				   "abc: 1\n"
+				   "def: 2");
+	EXPECT_EQ(e.command, "FOO");
+	EXPECT_TRUE(e.args.empty());
+	EXPECT_FALSE(e.headers.empty());
+	EXPECT_EQ(std::distance(e.headers.begin(), e.headers.end()), 2u);
+	EXPECT_EQ(e.headers.find("abc")->second, "1");
+	EXPECT_EQ(e.headers.find("def")->second, "2");
 }
 
 TEST(Parser, Full)
 {
-    static constexpr char payload[] =
-        "FOO one two three\n"
-        "abc: 1\n"
-        "def: 2\n"
-        "\n"
-        "\x00\x01\x02\x03";
-    const auto e = ParseEntity({payload, sizeof(payload) - 1});
-    EXPECT_EQ(e.command, "FOO");
-    EXPECT_FALSE(e.args.empty());
-    EXPECT_EQ(e.args.front(), "one");
-    EXPECT_EQ(*std::next(e.args.begin()), "two");
-    EXPECT_EQ(*std::next(e.args.begin(), 2), "three");
-    EXPECT_EQ(std::next(e.args.begin(), 3), e.args.end());
-    EXPECT_FALSE(e.headers.empty());
-    EXPECT_EQ(std::distance(e.headers.begin(), e.headers.end()), 2u);
-    EXPECT_EQ(e.headers.find("abc")->second, "1");
-    EXPECT_EQ(e.headers.find("def")->second, "2");
+	static constexpr char payload[] =
+		"FOO one two three\n"
+		"abc: 1\n"
+		"def: 2\n"
+		"\n"
+		"\x00\x01\x02\x03";
+	const auto e = ParseEntity({payload, sizeof(payload) - 1});
+	EXPECT_EQ(e.command, "FOO");
+	EXPECT_FALSE(e.args.empty());
+	EXPECT_EQ(e.args.front(), "one");
+	EXPECT_EQ(*std::next(e.args.begin()), "two");
+	EXPECT_EQ(*std::next(e.args.begin(), 2), "three");
+	EXPECT_EQ(std::next(e.args.begin(), 3), e.args.end());
+	EXPECT_FALSE(e.headers.empty());
+	EXPECT_EQ(std::distance(e.headers.begin(), e.headers.end()), 2u);
+	EXPECT_EQ(e.headers.find("abc")->second, "1");
+	EXPECT_EQ(e.headers.find("def")->second, "2");
 }
