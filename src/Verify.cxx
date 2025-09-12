30bb657cdc2ef20542ae089bbf838a3d3df45230
Max Kellermann <max.kellermann@ionos.com>
2025-09-12 17:36:50 +0200

    Verify: un-inline CheckCommand()

---
+++ src/Verify.cxx
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <max.kellermann@ionos.com>
+
+#include "Verify.hxx"
+#include "util/StringVerify.hxx"
+
+#include <stdexcept>
+
+void
+CheckCommand(std::string_view s)
+{
+	if (s.empty())
+		throw std::runtime_error("Empty command");
+
+	if (!CheckChars(s, IsValidCommandChar))
+		throw std::runtime_error{"Malformed command"};
+}
--- src/Verify.hxx
+++ src/Verify.hxx
@@ -5,9 +5,7 @@
 #pragma once
 
 #include "util/CharUtil.hxx"
-#include "util/StringVerify.hxx"
 
-#include <stdexcept>
 #include <string_view>
 
 static constexpr bool
@@ -16,12 +14,5 @@ IsValidCommandChar(char ch)
 	return IsAlphaNumericASCII(ch) || ch == '_';
 }
 
-static void
-CheckCommand(std::string_view s)
-{
-	if (s.empty())
-		throw std::runtime_error("Empty command");
-
-	if (!CheckChars(s, IsValidCommandChar))
-		throw std::runtime_error{"Malformed command"};
-}
+void
+CheckCommand(std::string_view s);
