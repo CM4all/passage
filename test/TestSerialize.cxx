1b6713cf2f287fcdc438ab6b642cffdf8e94fa4f
Max Kellermann <max.kellermann@ionos.com>
2025-09-12 17:53:20 +0200

    test/TestSerialize: new unit test for Entity::Serialize()

---
+++ test/TestSerialize.cxx
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <max.kellermann@ionos.com>
+
+#include "Entity.hxx"
+
+#include <gtest/gtest.h>
+
+using std::string_view_literals::operator""sv;
+
+TEST(Serialize, JustCommand)
+{
+	const Entity e{
+		.command = "OK",
+	};
+
+	const auto result = e.Serialize();
+	EXPECT_EQ(result, "OK");
+}
+
+TEST(Serialize, CommandWithParameters)
+{
+	const Entity e{
+		.command = "FOO",
+		.args = {"one", "two"},
+	};
+
+	const auto result = e.Serialize();
+	EXPECT_EQ(result, "FOO one two");
+}
+
+TEST(Serialize, WithHeaders)
+{
+	const Entity e{
+		.command = "FOO",
+		.args = {"one", "two"},
+		.headers = {{"abc", "1"}, {"def", "2"}},
+	};
+
+	const auto result = e.Serialize();
+	EXPECT_EQ(result, "FOO one two\nabc:1\ndef:2\n");
+}
+
+TEST(Serialize, WithBinaryBody)
+{
+	const Entity e{
+		.command = "FOO",
+		.args = {"arg"},
+		.body = std::string{"\x00\x01\x02\x03"sv},
+	};
+
+	const auto result = e.Serialize();
+	EXPECT_EQ(result, "FOO arg\0\x00\x01\x02\x03"sv);
+}
+
+TEST(Serialize, Everything)
+{
+	const Entity e{
+		.command = "FOO",
+		.args = {"one", "two", "three"},
+		.headers = {{"abc", "1"}, {"def", "2"}},
+		.body = std::string{"\x00\x01\x02\x03"sv},
+	};
+
+	const auto result = e.Serialize();
+	EXPECT_EQ(result, "FOO one two three\nabc:1\ndef:2\n\0\x00\x01\x02\x03"sv);
+}
